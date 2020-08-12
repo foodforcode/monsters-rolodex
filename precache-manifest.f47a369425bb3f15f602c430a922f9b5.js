9c87256cf5ac2cb86b3d6225ee2d43b99f4004bb
Connie T <tsai.connie@yahoo.com>
2020-08-12 14:13:19 -0700

    Updates

---
+++ precache-manifest.f47a369425bb3f15f602c430a922f9b5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f877c210eece94dd96fc2267e47b8a99",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "fd7b765aa3fa5c3e8eac",
+    "url": "/monsters-rolodex/static/css/main.f2a6f5b0.chunk.css"
+  },
+  {
+    "revision": "0c93387514e954bdf57e",
+    "url": "/monsters-rolodex/static/js/2.605ea4c5.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters-rolodex/static/js/2.605ea4c5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fd7b765aa3fa5c3e8eac",
+    "url": "/monsters-rolodex/static/js/main.0ee529ab.chunk.js"
+  },
+  {
+    "revision": "9b59177dc23ec2a3d1f7",
+    "url": "/monsters-rolodex/static/js/runtime-main.fa9629a0.js"
+  }
+]);
